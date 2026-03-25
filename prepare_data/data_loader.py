6655823ef76dfc5df707ba6e469c14d8806cab20
Noemie <majerusnoemie@gmail.com>
2026-03-25 16:18:19 +0100

    add prepare_data module

--- prepare_data/data_loader.py
+++ prepare_data/data_loader.py
@@ -1,15 +1,20 @@
 import json
 import pandas as pd
 
-def load_coco_json(json_path):
-    """Reads the COCO JSON file and returns the raw dictionary."""
-    with open(json_path, 'r') as f:
+def load_coco_json():
+    with open("Data/raw/_annotations.coco.json", "r") as f:
         data = json.load(f)
     return data
 
-def get_dataframes(coco_data):
-    """Converts COCO data into two clean Pandas DataFrames."""
-    df_images = pd.DataFrame(coco_data['images'])
-    df_annotations = pd.DataFrame(coco_data['annotations'])
-    
+def get_dataframes(data):
+    df_images = pd.DataFrame(data["images"])
+    df_annotations = pd.DataFrame(data["annotations"])
     return df_images, df_annotations
+
+
+# TEST (important sinon ton fichier sert à rien)
+data = load_coco_json()
+df_images, df_annotations = get_dataframes(data)
+
+print(df_images.head())
+print(df_annotations.head())
